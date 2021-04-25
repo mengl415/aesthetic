45742c1201d3e846961e9c4652c7b0a4c9186c35
Lily <mengl415@newschool.edu>
2021-04-25 17:06:09 -0500

    added assets for some, added css and html pages
    added images for cottage, dark aca, devil, fairy; favicons for cottage and dark aca

---
+++ script.js
@@ -0,0 +1,39 @@
+function cottagePage(){
+    window.open("cottageCore.html", "_blank");
+}
+
+function darkAcaPage(){
+    window.open("darkAca.html", "_blank");
+}
+
+function devilPage(){
+    window.open("devilCore.html", "_blank");
+}
+
+function eGirlPage(){
+    window.open("eGirl.html", "_blank");
+}
+
+function eGirlPage(){
+    window.open("eGirl.html", "_blank");
+}
+
+function fairyPage(){
+    window.open("fairyCore.html", "_blank");
+}
+
+function loFiPage(){
+    window.open("loFi.html", "_blank");
+}
+
+function moriKeiPage(){
+    window.open("moriKei.html", "_blank");
+}
+
+function spacePage(){
+    window.open("spaceCore.html", "_blank");
+}
+
+function vaporwavePage(){
+    window.open("vaporwave.html", "_blank");
+}

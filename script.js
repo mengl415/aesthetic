5dc5db13c4d97e13e17f634ac5fd56d9d7653e2a
Lily <mengl415@newschool.edu>
2021-05-04 18:18:36 -0500

    trying to make rectangles in javascript, trying to make vertical accordion

--- script.js
+++ script.js
@@ -41,3 +41,35 @@ function vaporwavePage(){
 function homePage(){
     window.open("index.html", "_blank");
 }
+
+var ccArray = [
+    "#EDC726", "#8B5B2B", "#EBD2B7", "#E9DF96", "#D18B89", "#FEEAC5",
+    "#844638", "#9C774C", "#878326"
+];
+
+var r1 = document.getElementById("rect1");
+var r2 = document.getElementById("rect2");
+var r3 = document.getElementById("rect3");
+var r4 = document.getElementById("rect4");
+var r5 = document.getElementById("rect5");
+
+var ccRect = [r1, r2, r3, r4, r5];
+
+var ccColor = ccArray[Math.floor(Math.random()*9)];
+console.log(ccColor);
+
+var ccRectIndex = Math.floor(Math.random()*5);
+console.log(ccRectIndex);
+
+var newRect = ccRect[ccRectIndex];
+
+function ccRectColor(){
+    newRect.style.backgroundColor = ccColor;
+    newRect.style.display = "none";
+}
+
+
+document.getElementById("cottageCore").addEventListener("mouseover", ccRectColor());
+
+
+
